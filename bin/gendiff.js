9c2aaa06673694b46bf5b6d457352d0c709dc21f
имя фамилия <polinalisichkina@icloud.com>
2023-03-22 14:50:28 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
-import { Command } from 'commander/esm.mjs';
+import { Command } from 'commander';
 const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  
 program.parse();
