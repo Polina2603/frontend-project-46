16b38f937e307b67158a779a6d17bafe122b0ac7
имя фамилия <polinalisichkina@icloud.com>
2023-03-25 19:51:14 +0300

    step4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { readFileSync } from 'node:fs';
 import path from 'node:path';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import _ from 'lodash';
 
 const makeString = (arr) => {
@@ -26,11 +27,11 @@ const getDiff = (filepath1, filepath2) => {
   const keys2 = Object.keys(file2); // Массив ключей второго файла
   const keys = _.union(keys1, keys2).sort(); // Массив ключей обоих файлов в алфавитном порядке
   const result = keys.reduce((current, key) => {
-    if (keys1.includes(key) && !(keys2.includes(key))) { // Если ключ есть в 1-м, но нет во 2-м файле
+    if (keys1.includes(key) && !(keys2.includes(key))) {
       current.push(`- ${key}: ${file1[key]}`);
       return current;
     }
-    if (!(keys1.includes(key)) && keys2.includes(key)) { // Если ключ есть во 2-м, но нет во 1-м файле
+    if (!(keys1.includes(key)) && keys2.includes(key)) {
       current.push(`+ ${key}: ${file2[key]}`);
       return current;
     }
