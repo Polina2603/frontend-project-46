3b2e79368ada71e9b349f9686c220e14ffc6cdb8
имя фамилия <polinalisichkina@icloud.com>
2023-03-27 14:24:55 +0300

    4

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -15,5 +15,6 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '_fixtures_', fi
 const comparisonResult = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
 
 test('get difference / json', () => {
-  expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(comparisonResult);
-  });
+    expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(comparisonResult);
+    expect(getDiff(getFixturePath('emptyFile1.json'), getFixturePath('emptyFile2.json'))).toEqual('Files are empty!');
+});
